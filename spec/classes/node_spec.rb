51c91bf39fe360a97d0e699aa6c4f155235b133d
Jeremy Thurgood <jeremy@praekelt.com>
2016-02-19 11:08:21 +0200

    Experiment with rantly.

--- spec/classes/node_spec.rb
+++ spec/classes/node_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'rantly'
 
 describe 'xylem::node' do
   on_supported_os.each do |os, facts|
@@ -28,6 +29,34 @@ describe 'xylem::node' do
           is_expected.to contain_service('xylem')
             .with_ensure('running')
             .that_subscribes_to('File[/etc/xylem/xylem.yml]')
+        end
+      end
+
+      Rantly.map(20) do
+        {
+          :gluster => boolean,
+          :gluster_mounts => (1..range(1, 4)).map { |i| "/data/brick#{i}" },
+          :gluster_nodes => (1..range(1, 4)).map { |i| "gfs#{i}.local" },
+          :gluster_replica => choose(false, 2, 3, 4, 5),
+          :gluster_stripe => choose(false, 2, 3, 4, 5),
+        }
+      end.each do |args|
+        gq = {
+          'name' => 'gluster',
+          'plugin' => 'seed.xylem.gluster',
+          'gluster_mounts' => args[:gluster_mounts],
+          'gluster_nodes' => args[:gluster_nodes],
+        }
+        gq['gluster_replica'] = args[:gluster_replica] if args[:gluster_replica]
+        gq['gluster_stripe'] = args[:gluster_stripe] if args[:gluster_stripe]
+        queues = if args[:gluster]; [gq] else nil end
+
+        describe "when configured with #{args.inspect}" do
+          let(:params) { args }
+          it do
+            is_expected.to contain_file('/etc/xylem/xylem.yml')
+              .with_content(match_yaml({'queues' => queues}))
+          end
         end
       end
 
