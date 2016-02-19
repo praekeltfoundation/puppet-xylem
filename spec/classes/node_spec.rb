aa180a1197c70c73f9f82b30622e8c8f3cedb8df
Jeremy Thurgood <jeremy@praekelt.com>
2016-02-19 12:01:10 +0200

    Add tests for configuring both gluster and postgres.

--- spec/classes/node_spec.rb
+++ spec/classes/node_spec.rb
@@ -142,6 +142,70 @@ describe 'xylem::node' do
         end
       end
 
+      describe 'when gluster and postgres are configured' do
+        let(:params) { {:gluster => true, :postgres => true} }
+
+        it do
+          is_expected.to contain_file('/etc/xylem/xylem.yml')
+            .with_content(match_yaml({
+                'queues' => contain_exactly({
+                    'name' => 'gluster',
+                    'plugin' => 'seed.xylem.gluster',
+                    'gluster_mounts' => nil,
+                    'gluster_nodes' => nil,
+                  }, {
+                    'name' => 'postgres',
+                    'plugin' => 'seed.xylem.postgres',
+                    'key' => nil,
+                    'servers' => [{
+                        'hostname' => nil,
+                        'username' => nil,
+                      }]
+                  })
+              }))
+        end
+
+        describe 'with all params' do
+          let(:params) do
+            {
+              :gluster => true,
+              :gluster_mounts => ['/data/brick1', '/data/brick2'],
+              :gluster_nodes => ['gfs1.local', 'gfs2.local'],
+              :gluster_replica => 2,
+              :gluster_stripe => 3,
+              :postgres => true,
+              :postgres_host => 'db.local',
+              :postgres_user => 'pguser',
+              :postgres_password => 'pgpass',
+              :postgres_secret => 'pgsec',
+            }
+          end
+
+          it do
+            is_expected.to contain_file('/etc/xylem/xylem.yml')
+              .with_content(match_yaml({
+                  'queues' => contain_exactly({
+                      'name' => 'gluster',
+                      'plugin' => 'seed.xylem.gluster',
+                      'gluster_mounts' => ['/data/brick1', '/data/brick2'],
+                      'gluster_nodes' => ['gfs1.local', 'gfs2.local'],
+                      'gluster_replica' => 2,
+                      'gluster_stripe' => 3,
+                    }, {
+                      'name' => 'postgres',
+                      'plugin' => 'seed.xylem.postgres',
+                      'key' => 'pgsec',
+                      'servers' => [{
+                          'hostname' => 'db.local',
+                          'username' => 'pguser',
+                          'password' => 'pgpass',
+                        }]
+                    })
+                }))
+          end
+        end
+      end
+
       describe 'when package_ensure is purged' do
         let(:params) { {:package_ensure => 'purged'} }
         it do
