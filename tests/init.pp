1e25c7651337d0932b730dcf8a31dc669f1e8a7e
Colin Alston <colin.alston@gmail.com>
2016-02-24 10:07:04 +0200

    Fix merge error

--- tests/init.pp
+++ tests/init.pp
@@ -24,9 +24,5 @@ class {'xylem::node':
   postgres        => true,
   postgres_host   => 'localhost',
   postgres_user   => 'postgres',
-<<<<<<< HEAD
   postgres_secret => 'testkey'
-=======
-  postgres_secret => 'testkey',
->>>>>>> develop
 }
